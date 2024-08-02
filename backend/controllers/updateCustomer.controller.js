fa79adaf344ca66e980f2a77e5ac06f6b37520a2
deepdarshan21 <deepdarshan21@gmail.com>
2024-08-02 14:25:57 +0530

    feat: create or update user details

---
+++ backend/controllers/updateCustomer.controller.js
@@ -0,0 +1,47 @@
+
+const Customer = require('../models/customer.model');
+
+const updateCustomerDetails = async (res, req) => {
+    const { 
+        firstname,
+        lastname,
+        email,
+        pin, 
+        phonenumber,
+        creditAmount,
+        invoiceList,
+        wellfareNo,
+    } = req.body;
+
+    // find customer if already exits
+    const customer = await Customer.findOne({ wellfareNo });
+
+    if (!customer) {
+        customer = new Customer({
+            firstname,
+            lastname,
+            email,
+            pin,
+            phonenumber,
+            creditAmount,
+            invoiceList,
+            wellfareNo,
+        })
+    } else {
+        customer.firstname = firstname;
+        customer.lastname = lastname;
+        customer.email = email;
+        customer.phonenumber = phonenumber;
+        customer.pin = pin;
+        customer.creditAmount = creditAmount;
+        customer.invoiceList = invoiceList;
+        customer.wellfareNo = wellfareNo;
+    }
+
+    // Create or Update the customer to the database
+    const result = await customer.save();
+
+    res.status(200).json(result);
+
+    module.exports = {updateCustomerDetails};
+}
